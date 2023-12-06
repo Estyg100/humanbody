a90132cf2e7babfdbb91e81f8c318098bff05f4e
estyg100 <estygrunwald1@gmail.com>
2023-12-06 17:10:21 -0500

    1st commit on trial

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -4,6 +4,7 @@ drop table if exists body
 go
 create table dbo.body(
     id int not null identity primary key, 
+	creaturetype varchar(25) not null,
     bodypart varchar(100) not null, 
     num int not null
 )
